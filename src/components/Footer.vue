580441f2fa1f3d819d55fa5f343de9ef73920824
Gael <gbouvyer@gmail.com>
2020-11-16 16:01:54 +0100

    commit a02

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer>
 		<div class="container-fluid">
-			<h6 class="align-center">2020 La galerie d'Anne - Made with ♥ by <a class="link link-theme" href="https://www.thebowyer.design/" target="_blank">Gael</a></h6>
+			<h6 class="align-center">2020 Anne's Gallery - Made with ♥ by <a class="link link-theme" href="https://www.thebowyer.design/" target="_blank">The Bowyer</a></h6>
 		</div>
 	</footer>
 </template>
